e53ba9ba84ad2f13d4e8bf28f627cf3f646ad404
shamsharoon <shamsuddin.haroon@ontariotechu.net>
2024-07-05 17:22:00 -0400

    ig fix 2

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,11 @@ import Portfolio from "./components/Portfolio"
 import Testimonials from "./components/Testimonials"
 import Pricing from "./components/Pricing"
 import Footer from "./components/Footer"
-import useViewportHeight from "./components/useViewportHeight"
 
 // TODO: Fix footer so that the phone# and email are not underlined and ontop of each other
 // TODO: It Should say like book with me now above the footer
 
 function App() {
-  useViewportHeight();
   return (
     <>
       <Navbar />
